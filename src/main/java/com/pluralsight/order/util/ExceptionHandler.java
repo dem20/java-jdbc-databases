c55d5b92b0144068d6260410300fb54fa7f119a5
dem20 <de_ni_se1@hotmail.com>
2020-10-26 20:37:28 -0600

    module 3 complete

--- src/main/java/com/pluralsight/order/util/ExceptionHandler.java
+++ src/main/java/com/pluralsight/order/util/ExceptionHandler.java
@@ -12,6 +12,9 @@ public class ExceptionHandler {
      * @param sqlException Exception from which information will be extracted
      */
     public static void handleException(SQLException sqlException) {
-
+    	System.out.println("Error code: " + sqlException.getErrorCode());
+        System.out.println("SQL state: " + sqlException.getSQLState());
+        System.out.println("Message: " + sqlException.getMessage());
+        sqlException.printStackTrace();
     }
 }
